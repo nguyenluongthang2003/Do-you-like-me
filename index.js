f788bf43dfbf12f2c3a5a46e659a131619bcb053
nguyenluongthang <114991938+nguyenluongthang@users.noreply.github.com>
2023-12-06 21:52:42 +0700

    Do you like me

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ yesBtn.addEventListener("click", () => {
     question.innerHTML = "Aaaaa, I like you too"; 
     gif.src ="https://i.pinimg.com/originals/e3/8c/cf/e38ccf14293cf5319d4240952e5e133b.gif"; 
 })
-noBtn.addEventListener("mouseover", () => { 
+noBtn.addEventListener("click", () => { 
     const noBtnRect = noBtn.getBoundingClientRect(); 
     const maxX = window.innerWidth - noBtnRect.width; 
     const maxY = window.innerHeight - noBtnRect.height; 
     
     const randomX = Math.floor(Math.random() * maxX) ; 
     const randomY = Math.floor(Math.random() * maxY); 
+
     noBtn.style.left = randomX + "px"; 
-    noBtn.style.top = randomY + " px";
+    noBtn.style.top = randomY + "px";
 
 })
